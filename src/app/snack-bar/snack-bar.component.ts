c4366b1b0ab50b11bad1930cf3234d28ba2c3741
Dimitris Stylianou <dimitrisstylianou@proton.me>
2025-01-08 17:07:21 +0200

    Create snack-bar component

---
+++ src/app/snack-bar/snack-bar.component.ts
@@ -0,0 +1,16 @@
+// src/app/snack-bar/snack-bar.component.ts
+
+import {Component, inject} from '@angular/core';
+import {MAT_SNACK_BAR_DATA} from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-snack-bar',
+  imports: [],
+  templateUrl: './snack-bar.component.html',
+  styleUrl: './snack-bar.component.scss'
+})
+export class SnackBarComponent {
+
+  message: string = inject(MAT_SNACK_BAR_DATA);
+
+}
