6c21d26e153fdd9488e0b1f72ec69db89e36c351
Dimitris Stylianou <dimitrisstylianou@proton.me>
2025-01-09 12:50:59 +0200

    Update navbar component
    - Add functionality for creating new quests.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,6 @@ import {Routes} from '@angular/router';
 import {QuestsComponent} from './quests/quests.component';
 
 export const routes: Routes = [
+  {path: '', redirectTo: 'quests', pathMatch: 'full'},
   {path: 'quests', component: QuestsComponent},
 ];
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -4,6 +4,12 @@ import {Component} from '@angular/core';
 import {MatTabsModule} from '@angular/material/tabs';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {TitleCasePipe} from '@angular/common';
+import {DialogData} from '../dialog-data';
+import {Difficulty} from '../difficulty';
+import {Status} from '../status';
+import {DialogService} from '../dialog.service';
+import {QuestManagerService} from '../quest-manager.service';
+import {SnackBarService} from '../snack-bar.service';
 
 @Component({
   selector: 'app-navbar',
@@ -14,7 +20,33 @@ import {TitleCasePipe} from '@angular/common';
 export class NavbarComponent {
 
   readonly title = 'Adventure Quest Manager';
-  readonly links = ['dashboard', 'quests', 'about'];
+  readonly links = ['quests', 'about'];
   activeLink = this.links[0];
+
+
+  constructor(private readonly dialogService: DialogService,
+              private readonly snackBarService: SnackBarService,
+              private readonly questManagerService: QuestManagerService) {
+  }
+
+  openNewQuestDialog() {
+    const dialogData: DialogData = {
+      heading: 'New Quest',
+      positiveButtonText: 'Create',
+      quest: {
+        id: 0,
+        title: '',
+        description: '',
+        difficulty: Difficulty.EASY,
+        status: Status.INCOMPLETE
+      }
+    };
+
+    this.dialogService.openDialog(dialogData, quest => {
+      if (!quest) return;
+      this.questManagerService.createQuest(quest);
+      this.snackBarService.openSnackBar('Quest created successfully');
+    });
+  }
 
 }
